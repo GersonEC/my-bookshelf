e1ade880b92b5055e1d06a86ac3c08017435e097
GersonEC <gersonenriquez94@gmail.com>
2021-10-02 19:27:23 +0200

    Add Notification component.

--- app/core/components/Category/Category.tsx
+++ app/core/components/Category/Category.tsx
@@ -3,6 +3,9 @@ import { AiFillDelete } from "react-icons/ai"
 import { Category as CategoryModel } from "db"
 import { useMutation } from "@blitzjs/core"
 import deleteCategory from "./mutations/deleteCategory"
+import { Button, notification, Divider, Space } from "antd"
+import { RadiusUprightOutlined } from "@ant-design/icons"
+import "antd/dist/antd.css"
 
 interface CategoryProps {
   category: CategoryModel
@@ -16,8 +19,21 @@ export default function Category({ category }: CategoryProps) {
     console.log(`Categoria eliminata ${category}`)
   }
 
+  const openNotification = (placement) => {
+    notification.info({
+      message: `Notification ${placement}`,
+      description:
+        "This is the content of the notification. This is the content of the notification. This is the content of the notification.",
+      placement,
+    })
+  }
+
   return (
     <div>
+      <Button type="primary" onClick={() => openNotification("topRight")}>
+        <RadiusUprightOutlined />
+        topRight
+      </Button>
       <div className={styles.category_action}>
         <AiFillDelete title="Eliminare Categoria" onClick={() => onCategoryDelete(category.id)} />
       </div>
