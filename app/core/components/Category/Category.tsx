4a3d9f39b813dfcdea22365027cf93681a42dd1b
GersonEC <gersonenriquez94@gmail.com>
2021-10-02 18:29:28 +0200

    Add delete icon on Category.

--- app/core/components/Category/Category.tsx
+++ app/core/components/Category/Category.tsx
@@ -1,13 +1,23 @@
 import styles from "./Category.module.css"
+import { AiFillDelete } from "react-icons/ai"
 
 interface CategoryProps {
   name: string
 }
 
 export default function Category({ name }: CategoryProps) {
+  const onCategoryDelete = () => {
+    alert("Categoria eliminata")
+  }
+
   return (
-    <div className={styles.category}>
-      <h3 className={styles.name}>{name}</h3>
+    <div>
+      <div className={styles.category_action}>
+        <AiFillDelete title="Eliminare Categoria" onClick={onCategoryDelete} />
+      </div>
+      <div className={styles.category_body}>
+        <h3 className={styles.name}>{name}</h3>
+      </div>
     </div>
   )
 }
